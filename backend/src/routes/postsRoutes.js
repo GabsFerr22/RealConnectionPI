18a789a87aeb1d2d081b85850fb6e7cf33b51bdf
GabsFerr22 <jgabsprofissional@gmail.com>
2025-05-29 16:13:46 -0300

    Ajeitando o POSTCONTROLLERS

--- backend/src/routes/postsRoutes.js
+++ backend/src/routes/postsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { upload, createPost, getPosts } = require('../controllers/postsController');
+const { upload, createPost, getPosts } = require('../controllers/postsControllers');
 
 router.post('/posts', upload.single('image'), createPost);
 router.get('/posts', getPosts);
